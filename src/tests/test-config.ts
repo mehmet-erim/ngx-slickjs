ca3885accb87ac9e032048e5ec46cd1c7faecd42
mehmet-erim <bilgi@mehmeterim.com>
2019-04-30 18:27:25 +0300

    Add author line package json files Fix test-config

--- src/tests/test-config.ts
+++ src/tests/test-config.ts
@@ -1,19 +1,25 @@
 import { FormsModule } from "@angular/forms";
 import { SlickContainerDirective, SlickItemDirective } from "../lib/directives";
 import { TestComponent } from "./test.component";
+import { SLICK_LINKS, linkOptionsFactory } from "../lib/tokens";
 
 export default {
   imports: [],
   declarations: [SlickContainerDirective, SlickItemDirective, TestComponent],
   providers: [
     {
-      provide: "SLICK_LINKS",
+      provide: SLICK_LINKS,
       useValue: {
         jquery: "https://code.jquery.com/jquery-3.4.0.min.js",
         slickJs: "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js",
         slickCss: "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css",
         slickThemeCss: "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css",
       },
+    },
+    {
+      provide: "slick-links",
+      useFactory: linkOptionsFactory,
+      deps: [SLICK_LINKS],
     },
   ],
 };
